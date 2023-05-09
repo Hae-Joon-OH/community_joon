3ae72e8f5805bfc2ff747fee6293d5a52a8206c5
JJoon <chipp0604@gmail.com>
2023-05-09 15:03:39 +0900

    ROOT파일 변경

--- src/main/resources/static/board/js/noticeInsert.js
+++ src/main/resources/static/board/js/noticeInsert.js
@@ -14,7 +14,8 @@ sBtn.addEventListener('click', function(){
         success: function(responseJson) {
             responseJson = JSON.parse(responseJson);
             alert("글 작성에 성공하였습니다.")
-            window.location.href="/board/noticeDetail?boardNum=" + responseJson['result'];
+            // window.location.href="/board/noticeDetail?boardNum=" + responseJson['result'];
+            window.location.href="/board/notice";
         },
         error: function () {
             alert("서버와 연결이 끊겼습니다.")
